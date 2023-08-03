11b7220496c523382b0669d06d8bb08f9b5ae257
andrewmalley725 <andrew.alley99@gmail.com>
2023-08-03 17:22:49 -0500

    working production build

---
+++ chat-app-schema.sql
@@ -0,0 +1,41 @@
+use chat_app;
+
+drop table person;
+drop table room;
+drop table message;
+drop table user_rooms;
+
+create table person(
+	userid int primary key auto_increment,
+    username varchar(20),
+    firstname varchar(20),
+    lastname varchar(20),
+    password varchar(64),
+    email varchar (100)
+);
+
+create table room(
+	roomid int primary key auto_increment,
+    room_name varchar(20)
+);
+
+create table user_rooms(
+	userroomid int primary key auto_increment,
+	userid int references person,
+    roomid int references room
+);
+
+create table message(
+	messageid int primary key auto_increment,
+    userid int references person,
+    roomid int references room,
+    content varchar(200),
+    datesent datetime
+);
+
+create index userRoom on user_rooms(userid);
+create index roomUser on user_rooms(roomid);
+create index userMessage on message(userid);
+create index roomMessage on message(roomid);
+
+
--- server/app.js
+++ server/app.js
@@ -10,16 +10,16 @@ const app = express();
 
 app.use(cors());
 
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-//   });
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 
 app.use(express.urlencoded());
 
 app.use('/api', apiRouter);
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
 
 module.exports = app;
